d4971d8e1d931293694078afad97fa349923b766
Fernanda182 <ferchus182@hotmail.com>
2026-01-11 22:46:06 -0500

    Update Triangulo_area.py

--- Triangulo_area.py
+++ Triangulo_area.py
@@ -1,6 +1,6 @@
 # Programa para calcular el área de un triángulo
 # Solicita al usuario la base y la altura, calcula el área y muestra el resultado.
-# Autor: Tu nombre
+# Autor: Fernanda Vaca
 
 def calcular_area_triangulo(base: float, altura: float) -> float:
     return (base * altura) / 2
@@ -16,4 +16,5 @@ es_mayor_a_20: bool = area > 20
 
 print(f"\nEl área del triángulo es: {area:.2f} cm²")
 print(f"¿El área es mayor a 20 cm²? {es_mayor_a_20}")
+
 
